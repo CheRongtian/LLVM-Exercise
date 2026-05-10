31e5a91fe65a1a65b18e3aa8e4f0566e33fdf9c4
CheRongtian <145754269+CheRongtian@users.noreply.github.com>
2026-05-11 01:11:25 +0800

    do basic preparation for codegen, the IR part, need further fix for the content of the head file (maybe combine all into one head file)

---
+++ codegen.hpp
@@ -0,0 +1,22 @@
+#ifndef CODEGEN_HPP
+#define CODEGEN_HPP
+
+#include "AST.hpp"
+#include "lexer.hpp"
+#include "parser.hpp"
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constant.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+using namespace llvm;
+
+#endif
